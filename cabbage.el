64343c5581360f72076f80129f5692ee7cec9c8d
Ji Luo <weiqiyiji@gmail.com>
2014-11-30 22:17:28 +0800

    Disable perspective in Emacs 24.4

--- cabbage.el
+++ cabbage.el
@@ -26,7 +26,7 @@
 (load (concat cabbage-repository "lib/bundles/framework"))
 (load (concat cabbage-repository "lib/defun"))
 (load (concat cabbage-repository "lib/setup"))
-(load (concat cabbage-repository "lib/perspective"))
+;; (load (concat cabbage-repository "lib/perspective"))
 (load (concat cabbage-repository "lib/compilation"))
 (load (concat cabbage-repository "lib/modes"))
 
